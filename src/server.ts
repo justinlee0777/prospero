68816c9da40486561d78963aceba2fa0c247f03a
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-05-02 13:09:56 -0400

    WIP: Light contract testing for server exports

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import PagesBuilder from './pages.builder';
 
 import Pages from './pages';
 
-export * from './loaders/public-api';
-export * from './processors/public-api';
+export { LoaderBuilder } from './loaders/public-api';
+export { HTMLProcessor, IndentProcessor } from './processors/public-api';
 export { PagesBuilder, Pages };
