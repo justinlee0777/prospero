7b77f19f86f05ce21905cd380d92e4af1e5b65f5
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-07-14 12:32:49 -0400

    HTMLTransformer removes the beginning and ending spaces in block-level elements; they confuse the parser, which counts them, though the browser actually ignores them. This also stops confusion with the NewlineTransformer

--- src/transformers/html/html.transformer.ts
+++ src/transformers/html/html.transformer.ts
@@ -1,4 +1,6 @@
+import blockLevelTags from '../../parsers/html/block-level-tags.const';
 import createHTMLRegex from '../../regexp/html.regexp';
+import Transformer from '../models/transformer.interface';
 import HTMLTransformerOptions from './html-transformer-options.interface';
 
 interface HTMLTransformerConfig {
@@ -13,7 +15,11 @@ interface TransformTag {
  * Transform certain HTML tags into a tag compatible with the parser.
  * For example, h1..6 will be converted to <span> with the appropriate font size.
  */
-export default class HTMLTransformer {
+export default class HTMLTransformer implements Transformer {
+  readonly forHTML = true;
+
+  private readonly blockLevelTags = blockLevelTags;
+
   private headings: Map<string, TransformTag> = new Map([
     [
       'h1',
@@ -83,6 +89,29 @@ export default class HTMLTransformer {
         this.config.fontSize * 2
       }px;">${content}</div>`;
     });
+
+    text = this.blockLevelTags.reduce((finalText, tag) => {
+      /*
+       * Capture group: The opening tag for a block-level element.
+       * After the capture group is at least one space character.
+       * The regex removes the unnecessary space in a block-level element which is ignored (based on
+       * 'white-space: normal').
+       */
+      const openingSpaces = new RegExp(`(<${tag}[^\\/]*?>)\\s+`, 'g');
+
+      /*
+       * First capture group: any end of a tag.
+       * Second capture group: the end of a block-level element.
+       * In-between is at least one space character.
+       * The regex removes the unnecessary space at the end of a block-level element ONLY if the element
+       * is nested.
+       */
+      const endingSpaces = new RegExp(`(<\\/.*?>)\\s+(<\\/${tag}>)`, 'g');
+
+      return finalText
+        .replaceAll(openingSpaces, '$1')
+        .replaceAll(endingSpaces, '$1$2');
+    }, text);
 
     return text;
   }
